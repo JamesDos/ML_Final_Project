f03d9526c71f28aec9f7c54d0a31453568930fd7
James Tu <jamestvvo@gmail.com>
2024-12-03 15:30:57 -0500

    update file paths for starter.py and submission.py

--- src/starter.py
+++ src/starter.py
@@ -1,10 +1,12 @@
 import pandas as pd
 
-train = pd.read_csv("/kaggle/input/cs-3780-5780-how-do-you-feel/train.csv")
+train = pd.read_csv("data/train.csv")
 train_text = train["text"]
 train_label = train["label"]
 
-test = pd.read_csv("/kaggle/input/cs-3780-5780-how-do-you-feel/test.csv")
+test = pd.read_csv("data/test.csv")
 test_id = test["id"]
 test_text = test["text"]
 
+print(train.head())
+print(test.head())
--- src/submission.py
+++ src/submission.py
@@ -3,5 +3,4 @@ import pandas as pd
 id = range(15000)
 prediction = range(15000)
 submission = pd.DataFrame({'id': id, 'label': prediction})
-submission.to_csv('/kaggle/working/submission.csv', index=False)
-
+submission.to_csv('submission/submission.csv', index=False)
